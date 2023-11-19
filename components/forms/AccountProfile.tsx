3015d7963ca739d19df9b252d7d7ba3cffc22d04
flukexp <flukelolza@gmail.com>
2023-11-19 11:13:46 +0700

    mongoose connected and implemented DBSchema

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 
 interface Props {
@@ -38,6 +40,8 @@ const AccountProfile = ({ user , btnTitle }) => {
 
     const [files, setFiles ] = useState<File[]>([])
     const { startUpload } = useUploadThing("media");
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -85,8 +89,22 @@ const AccountProfile = ({ user , btnTitle }) => {
           }
         }
 
-        // TODO: Update user profile
-    }
+        await updateUser({
+          userId: user.id,
+          username: values.username,
+          name: values.name,
+          bio: values.bio,
+          image: values.profile_photo,
+          path: pathname
+        });
+
+        if(pathname === '/profile/edit') {
+          router.back();
+        } else {
+          router.push('/');
+        }
+      }
+          
       
     return (
         <Form {...form}>
