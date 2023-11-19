f626857811f6793a5c6f6c5005dce1a78dd064dd
flukexp <flukelolza@gmail.com>
2023-11-20 00:02:10 +0700

    Community Page

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -24,8 +24,7 @@ function LeftSideBar() {
                         <Link 
                             href={link.route}
                             key={link.label}
-                            className={`leftsidebar_link $ 
-                            {isActive && 'bg-primary-500'}`}
+                            className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
                         >
                             <Image 
                                 src={link.imgURL}
